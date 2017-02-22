db4e3a325a69de7178ab69673fa9ffeb01cdf3a3
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2017-02-23 02:54:35 +0530

    History update

--- lib/history.zsh
+++ lib/history.zsh
@@ -3,8 +3,8 @@ if [ -z "$HISTFILE" ]; then
     HISTFILE=$HOME/.zsh_history
 fi
 
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=1000000
+SAVEHIST=1000000
 
 # Show history
 case $HIST_STAMPS in
@@ -18,7 +18,11 @@ setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_all_dups # ignore duplication command history list
 setopt hist_ignore_space
+setopt hist_save_no_dups
+setopt hist_verify
+setopt hist_reduce_blanks
 setopt hist_verify
 setopt inc_append_history
 setopt share_history # share command history data

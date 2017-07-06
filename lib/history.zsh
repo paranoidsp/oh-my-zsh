980c8a40fbdcf3d78b68dde0fe14fe27069a2c14
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2017-07-06 11:11:51 +0530

    Rolled back hist dup ignore

--- lib/history.zsh
+++ lib/history.zsh
@@ -16,11 +16,11 @@ esac
 
 setopt append_history
 setopt extended_history
-#setopt hist_expire_dups_first
-#setopt hist_ignore_dups # ignore duplication command history list
-#setopt hist_ignore_all_dups # ignore duplication command history list
+setopt hist_expire_dups_first
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_all_dups # ignore duplication command history list
 setopt hist_ignore_space
-#setopt hist_save_no_dups
+setopt hist_save_no_dups
 setopt hist_verify
 setopt hist_reduce_blanks
 setopt hist_verify

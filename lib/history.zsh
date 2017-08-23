7883e88435b6678421e9b7c76b39232d7cc40760
Karthikeya Viswanath <karthikeya@hasura.io>
2017-08-23 16:22:52 +0530

    Dups back in history

--- lib/history.zsh
+++ lib/history.zsh
@@ -17,10 +17,10 @@ esac
 setopt append_history
 setopt extended_history
 #setopt hist_expire_dups_first
-setopt hist_ignore_dups # ignore duplication command history list
-setopt hist_ignore_all_dups # ignore duplication command history list
-setopt hist_ignore_space
-setopt hist_save_no_dups
+#setopt hist_ignore_dups # ignore duplication command history list
+#setopt hist_ignore_all_dups # ignore duplication command history list
+#setopt hist_ignore_space
+#setopt hist_save_no_dups
 setopt hist_verify
 setopt hist_reduce_blanks
 setopt hist_verify
